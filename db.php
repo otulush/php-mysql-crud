10a0b680b2b90692f0a016bc60379d6f7f917173
otulush <119027294+otulush@users.noreply.github.com>
2024-10-08 20:29:14 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-$conn = mysqli_connect(
-  'localhost',
-  'root',
-  'password123',
-  'php_mysql_crud'
-) or die(mysqli_erro($mysqli));
+$host = getenv('MYSQL_CONTAINER');
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_ROOT_PASSWORD');
+$database = getenv('MYSQL_DATABASE');
+
+$conn = mysqli_connect($host, $user, $password, $database) or die(mysqli_error($conn));
 
 ?>
